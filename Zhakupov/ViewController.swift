033c0588bab1c8a101266bc67e86fd64f840586a
MuratZhakupov <murat.zhakup@mail.ru>
2020-02-25 13:09:08 +0200

    Initial Commit

---
+++ Zhakupov/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zhakupov
+//
+//  Created by Murat Zhakupov on 2/25/20.
+//  Copyright © 2020 Murat Zhakupov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
